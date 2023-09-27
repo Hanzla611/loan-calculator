42fafe044782af3746276ae1864b61c216080f1d
abu.hanzla@neosoftmail.com <abu.hanzla@neosoftmail.com>
2023-09-27 16:31:03 +0530

    loan calculator

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Product from "./components/Product";
+import ProductData from "./utils/mockdata";
+function App() {
+  const [jsonData, setJsonData] = useState([]);
+  const [finalArr, setFinalArr] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      const data = ProductData;
+      let updatedData = [];
+      for (let i in data) {
+        if (data[i].id) {
+          const updatedField = { ...data[i], amount: "", checked: false };
+          updatedData = [...updatedData, updatedField];
+        }
+      }
+      setJsonData(updatedData);
+    };
+    getData();
+  }, []);
+
+  return (
+    <div className="App ">
+      <div className="app-content">
+        <header className="App-header">
+          <h2>LOAN</h2>
+        </header>
+        <Product
+          jsonData={jsonData}
+          setJsonData={setJsonData}
+          setFinalArr={setFinalArr}
+          finalArr={finalArr}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
